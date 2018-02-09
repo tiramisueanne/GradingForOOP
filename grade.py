4f18a576e217e8804d6681d12c3def3921ef05d7
Drothknar <sueanne@cs.utexas.edu>
2018-02-08 19:29:20 -0600

    Removes file afterwards

--- grade.py
+++ grade.py
@@ -1,6 +1,7 @@
 import webbrowser
 import json
 import sys
+import os
 
 submission = json.load(sys.stdin);
 
@@ -8,3 +9,4 @@ print submission["Project #1"]["Name"], "'s Submission "
 webbrowser.open_new_tab(submission["Project #1"]["GitHub URL"])
 webbrowser.open_new_tab(submission["Project #1"]["Travis CI URL"])
 
+os.remove("./jsons/Project1.json");
